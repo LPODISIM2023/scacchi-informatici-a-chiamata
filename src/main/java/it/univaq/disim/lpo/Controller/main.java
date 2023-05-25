94aeda5cfa7d51b984b733c9ed311dc78afdb1b5
Stefano <stefano.rosini99@gmail.com>
2023-05-25 12:16:28 +0200

    Modifica main

--- src/main/java/it/univaq/disim/lpo/Controller/main.java
+++ src/main/java/it/univaq/disim/lpo/Controller/main.java
@@ -1,13 +1,18 @@
 package it.univaq.disim.lpo.Controller;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 import it.univaq.disim.lpo.Model.PartitaModel;
 import it.univaq.disim.lpo.Service.ServiceImpl.PartitaServiceImpl;
 
 public class main {
 
 	public static void main(String[] args) {
-		PartitaModel partita = new PartitaServiceImpl("Partita1");		
-		partita.scegliTipologiaPartita();
+		LocalDateTime dataOra = LocalDateTime.now();
+		DateTimeFormatter formattazioneData = DateTimeFormatter.ofPattern("dd-MM-yyyy-HH-mm-ss");
+		String dataFormattata = dataOra.format(formattazioneData);
+		PartitaModel partita = new PartitaServiceImpl("Partita-"+ dataFormattata, 0);
 	}
 
 }
